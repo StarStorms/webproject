b2d79ccd954be0c2498c721d1f945ed0cc782fbf
PletinckxJ <julien.pletinckx@hotmail.com>
2016-07-15 21:45:57 +0200

    lolkik

---
+++ Library/Include.lib.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julien
+ * Date: 15/07/2016
+ * Time: 21:40
+ */
+
+include "Library/Config.lib.php";
+include "Library/Database.lib.php";
+include "Library/Session.lib.php";
+include "Classes/Activation.class.php";
