629c940930839648bb6cc7aabb0ad43edb7673fd
PletinckxJ <julien.pletinckx@hotmail.com>
2016-07-15 22:22:03 +0200

    lol

--- index.php
+++ index.php
@@ -6,18 +6,21 @@
  * Time: 21:28
  */
 require "Library/Include.lib.php";
-$conf = parse_ini_file("config.ini.php");
+
 
 ?>
 
 <!DOCTYPE HTML>
 <head>
     <title> Site PMM 2016</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=windows-1252" />
+    <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="Style/bootstrap-3.3.6-dist/css/bootstrap.css" />
     <link rel="stylesheet" type="text/css" href="Style/privateCSS.css" />
 </head>
 <body>
+    <?php
+    $conf = parse_ini_file("config.ini.php");
+    ?>
     <header>
         <img src="<?php echo $conf['banniere']; ?>" alt="Bannière du site"><br>
         <h1><?php echo $conf['title']; ?></h1>
