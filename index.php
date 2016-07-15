40d8eecc6852e8b43fed0310e8ad6294a8fcab58
PletinckxJ <julien.pletinckx@hotmail.com>
2016-07-15 22:14:26 +0200

    test

--- index.php
+++ index.php
@@ -13,14 +13,14 @@ var_dump($conf);
 <!DOCTYPE HTML>
 <head>
     <title> Site PMM 2016</title>
-
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <link rel="stylesheet" type="text/css" href="Style/bootstrap-3.3.6-dist/css/bootstrap.css" />
     <link rel="stylesheet" type="text/css" href="Style/privateCSS.css" />
 </head>
 <body>
     <header>
-        <img src="<?php echo $conf['HEADER']['banniere']; ?>" alt="Bannière du site"><br>
-        <h1><?php echo $conf['HEADER']['title']; ?></h1>
-        <h2><?php echo $conf['HEADER']['Description']; ?></h2>
+        <img src="<?php echo $conf['banniere']; ?>" alt="Bannière du site"><br>
+        <h1><?php echo $conf['title']; ?></h1>
+        <h2><?php echo $conf['Description']; ?></h2>
     </header>
 </body>
