1e5bcdce4701e326100b4a17e10b067b71bdf2ee
PletinckxJ <julien.pletinckx@hotmail.com>
2016-07-15 22:35:38 +0200

    navbar

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * Time: 21:28
  */
 require "Library/Include.lib.php";
-
+startSession();
 
 ?>
 
@@ -26,4 +26,21 @@ require "Library/Include.lib.php";
         <h1><?php echo $conf['title']; ?></h1>
         <h2><?php echo $conf['Description']; ?></h2>
     </header>
+    <div id="navbar">
+        <ul>
+            <li><a href="index.php"> Accueil</a></li>
+            <li><a href="index.php?page=recherche"> Recherches</a></li>
+            <?php if (isset($_SESSION['Utilisateur'])) { ?>
+            <li><a href="index.php?page=enigme"> Enigmes</a></li>
+            <li><a href="index.php?page=administration"> Administration</a></li>
+            <li><a href="index.php?page=profil"> Profil</a></li>
+            <li><a href="index.php?page=deconnexion"> Déconnexion</a></li>
+            <?php } else { ?>
+            <li><a href="index.php?page=inscription"> Inscription</a></li>
+            <li><a href="index.php?page=connexion"> Connexion</a></li>
+            <?php } ?>
+
+
+        </ul>
+    </div>
 </body>
